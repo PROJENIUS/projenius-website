59d9f12ba9639e8ffafbdc1f3bfda94551ba13db
Nivas Kumar S <nivask457@gmail.com>
2026-05-12 21:19:08 +0530

    File Updated

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,7 +4,7 @@ import '../index.css';
 import '../assets/css/Product-page.css';
 export default function Product() {
   return (
-    <section className="header-wrap">
+    <section className="header-wrap"style={{backgroundImage:'linear-gradient(#1219297d), url(/images/projenius-banner.webp)'}}>
       <Header/>
       <div className="container title-section">
         <h1 className="page-title">Our Product</h1>
